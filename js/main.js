421e903a1fc8ba8e709aa50ceb74047aaf3ffaa1
Bruno Redzio <brunoredzio@hotmail.com>
2021-09-27 15:22:16 -0300

    clean code

--- js/main.js
+++ js/main.js
@@ -8,8 +8,6 @@ document.addEventListener('DOMContentLoaded', ()=>{
     fetchData()
 })
 
-
-
 items.addEventListener('click',e=>{
     addCarrito(e)
 })
@@ -18,20 +16,18 @@ const fetchData = async()=> {
     try{
         const res= await fetch('./json/cursos.json')
         const data = await res.json()
-        //console.log(data)
-        pintarCards(data)
+        cargarCursos(data)
     }catch(error){
         console.log(error)
     }
 }
 
-const pintarCards = data =>{
+const cargarCursos = data =>{
     data.forEach(producto=>{
         templateCard.querySelector('h4').textContent=producto.title
         templateCard.querySelector('h1').textContent=producto.precio
         templateCard.querySelector('.btn-outline-danger').dataset.id=producto.id
         const prueba = producto.contenido
-        
         
         for (const c of prueba){
             templateCard.querySelector('li').innerHTML +=`<li>${c}</li>`
@@ -73,122 +69,3 @@ const setCarrito = objeto=>{
     }
     
 }
-
-
-
-/*const pintarCarrito=()=>{
-    console.log(carrito)
-    let cart=document.getElementsByClassName("animal");
-    console.log(cart);
-    Object.values(carrito).forEach(producto=>{
-        cart[0].innerHTML=producto.nivel;  
-    })
-    
-     
-
-
-
-}*/
-
-
-
-
-
-/*
-const cursos=[];
-const carrito=[];
-
-class Curso{
-    constructor(nivel, precio, contenido){
-        this.nivel=nivel;
-        this.precio=precio;
-        this.contenido=contenido;
-    }s
-
-    agregarCurso(){
-        alert("Agregaste el curso al carrito");
-    }
-
-    quitarCurso(){
-        alert("Quitaste el curso del carrito");
-    }
-}
-
-const curso1={ nivel:1,
-               precio:1700,
-               contenido:"Cocción del arroz\nVinagreta Zu\nCortes del salmón"};
-
-
-const curso2= new Curso(2,1700,"Arroz con Tinta de Calamar");
-const curso3= new Curso(3,2800, "Todo lo que necesitás a un precio increible");
-
-cursos.push(curso1);
-cursos.push(curso2);
-cursos.push(curso3);
-
-do{
-    opcion=parseInt(prompt("Seleccione una opción para continuar: \n 1.Mostrar cursos disponibles \n 2.Agregar un curso al carrito \n 3.Quitar un curso del carrito \n 4.Mostrar Carrito"));
-    switch (opcion){
-        case 1:
-            alert("Cursos Disponibles");
-            for (const c of cursos){
-                alert("Nivel "+c.nivel+"\n\nContenido:\n"+c.contenido+"\n\nPrecio Por Mes\n$"+c.precio+"\n");
-                
-            };
-            break;
-        case 2:
-            numCurso=parseInt(prompt("Indique el nivel que quiere agregar"));
-            var z=0;
-            for (const c of cursos){
-                if(c.nivel==numCurso){
-                    carrito.push(c);
-                    alert("Se agregó el curso Nivel "+c.nivel+" al carrito");
-                    break;
-                }
-            };
-
-   
-            break;
-
-        case 3:
-            numCurso=parseInt(prompt("Indique el nivel que quiere quitar"));
-            var z=0;
-            for(const c of carrito){
-                if(c.nivel==numCurso){
-                    var i = carrito.indexOf(c);
-                    carrito.splice(i,1);
-                    alert("Se quitó el curso Nivel "+c.nivel+" al carrito");
-                }
-            }
-            break;
-        
-        case 4:
-        
-        let productos=document.getElementsByClassName("my-0-productos");
-        let m=0;
-        for (const c of carrito){
-            productos[m].innerHTML="Nivel "+c.nivel;   
-            m++;
-        }
-
-        let precios=document.getElementsByClassName("text-muted-precio");
-        let k=0;
-        for (const c of carrito){
-            precios[k].innerHTML="$"+c.precio;   
-            k++;
-        }
-            let aPagar="$"+Total(carrito);
-            document.getElementById("total").innerHTML=aPagar;
-            break;    
-        default:
-        alert("Opción invalida"); 
-            break;       
-    }    
-} while (opcion!=4);
-*/
-
-
-
-
-
-
